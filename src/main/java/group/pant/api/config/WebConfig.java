862f96973a8213b0699b04a88e54c3023d33b813
Paul Le Coz <plecoz.pro@gmail.com>
2025-04-14 11:37:48 +0200

    Fixed nonnull warning in cors config

--- src/main/java/group/pant/api/config/WebConfig.java
+++ src/main/java/group/pant/api/config/WebConfig.java
@@ -1,5 +1,6 @@
 package group.pant.api.config;
 
+import org.springframework.lang.NonNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -12,7 +13,7 @@ public class WebConfig {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/api/**")
                         .allowedOrigins("http://127.0.0.1:5500")
                         .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS")
